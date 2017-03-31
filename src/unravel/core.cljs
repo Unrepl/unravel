58fb944699ec88993d33ac292fe4ca19ee64cb40
Paulus Esterhazy <pesterhazy@gmail.com>
2017-03-31 12:59:45 +0200

    Stream EDN

--- src/unravel/core.cljs
+++ src/unravel/core.cljs
@@ -3,12 +3,51 @@
             [clojure.pprint :refer [pprint]]
             [lumo.core]
             [lumo.io :refer [slurp]]
-            [cljs.reader :refer [read-string]]))
+            [cljs.reader :refer [read-string]])
+  (:import [goog.string StringBuffer]))
 
 (def readline (js/require "readline"))
 (def net (js/require "net"))
 
 (def debug? (atom nil))
+
+;; ------
+
+(defn rstrip-one [s]
+  (clojure.string/replace s #"\n$" ""))
+
+(defn- reader-eof? [msg]
+  (or
+   (= "EOF while reading" msg)
+   (= "EOF while reading string" msg)))
+
+(defn- read-chars
+  [reader]
+  (let [sb (StringBuffer.)]
+    (loop [c (cljs.reader/read-char reader)]
+      (if-not (nil? c)
+        (do
+          (.append sb c)
+          (recur (cljs.reader/read-char reader)))
+        (str sb)))))
+
+(defn unblank [s]
+  (if (clojure.string/blank? s)
+    nil
+    s))
+
+(defn safe-read-string [s]
+  (let [reader (cljs.reader/push-back-reader s)
+        r (try
+            (cljs.reader/read reader true ::eof false)
+            (catch js/Error e
+              (if (reader-eof? (.-message e))
+                ::eof
+                (throw e))))]
+    (when (not= ::eof r)
+      [r (unblank (clojure.string/trim (read-chars reader)))])))
+
+;; ------
 
 (defn dbug [& args]
   (when @debug?
@@ -37,21 +76,7 @@
   (println "typed in:" (pr-str v)))
 
 (defn connect [host port on-connect on-data]
-  (let [cx (.Socket. net)]
-    (doto cx
-      (.connect port
-                host
-                (fn []
-                  (.setNoDelay cx true)
-                  (on-connect cx)))
-      (.on "error" (fn [err]
-                     (println "Socket error:" (pr-str err))
-                     (js/process.exit 1)))
-      (.on "data" (fn [data]
-                    (on-data (.toString data "utf8")))))))
-
-(defn rstrip-one [s]
-  (clojure.string/replace s #"\n$" ""))
+  )
 
 (defmulti obey first)
 
@@ -72,10 +97,36 @@
 (defmethod obey :default [command rl]
   (println "WARNING: unknown command" (pr-str command)))
 
-(defn did-receive [rl data]
-  (let [command (cljs.reader/read-string data)]
-    (dbug "CMD:" (pr-str command))
-    (obey command rl)))
+(defn did-receive [rl command]
+  (dbug "CMD:" (pr-str command))
+  (obey command rl))
+
+(defn edn-stream [stream on-read]
+  (let [buf (StringBuffer.)]
+    (.on stream "readable"
+         (fn []
+           (when-let [data (.read stream)]
+             (.append buf (.toString data "utf8"))
+             (when-let [[v rst] (safe-read-string (.toString buf))]
+               (on-read v)
+               (.clear buf)
+               (when rst
+                 (.unshift stream (js/Buffer.from rst "utf8")))))))))
+
+(defn consume-until [stream sentinel cb]
+  (let [buf (StringBuffer.)
+        !on-readable (atom nil)
+        on-readable (fn []
+                      (when-let [data (.read stream)]
+                        (.append buf (.toString data "utf8"))
+                        (let [s (.toString buf)
+                              idx (.indexOf s sentinel)]
+                          (when (not= -1 idx)
+                            (.removeListener stream "readable" @!on-readable)
+                            (cb)
+                            (.unshift stream (js/Buffer.from (subs s idx) "utf8"))))))]
+    (reset! !on-readable on-readable)
+    (.on stream "readable" on-readable)))
 
 (defn start [host port]
   (doseq [t '[unrepl/ns unrepl/raw unrepl/edn unrepl/param unrepl/... unrepl/object unrepl.java/class
@@ -84,22 +135,21 @@
   (let [rl (.createInterface readline #js{:input js/process.stdin
                                           :output js/process.stdout
                                           :prompt ">> "})
-        ready? (atom false)
-        client (connect host port
-                        (fn [cx]
-                          (.write cx (lumo.io/slurp "scripts/payload.clj")))
-                        (fn [data]
-                          (cond
-
-                            (.startsWith data "[:unrepl/hello")
-                            (do
-                              (reset! ready? true)
-                              (did-receive rl data))
-
-                            @ready?
-                            (did-receive rl data))))]
+        cx (.Socket. net)]
+    (doto cx
+      (.connect port
+                host
+                (fn []
+                  (.setNoDelay cx true)
+                  (.write cx (lumo.io/slurp "scripts/payload.clj"))))
+      (.on "error" (fn [err]
+                     (println "Socket error:" (pr-str err))
+                     (js/process.exit 1)))
+      (consume-until "[:unrepl/hello"
+                     (fn [] (edn-stream cx (fn [v]
+                                             (did-receive rl v))))))
     (.on rl "line" (fn [line]
-                     (.write client
+                     (.write cx
                              (str line "\n")
                              "utf8")))
     (.on rl "close" (fn [] (.exit js/process)))))
