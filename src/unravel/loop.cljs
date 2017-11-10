175a660ddbc04052ff428f287c6b523dc21fb8f0
Christophe Grand <christophe@cgrand.net>
2017-11-10 11:39:05 +0100

    First cut at a refactoring of the event handling.

--- src/unravel/loop.cljs
+++ src/unravel/loop.cljs
@@ -69,10 +69,6 @@
 (defmethod process :default [command]
   (ud/dbug :unknown-command command))
 
-(defn did-receive [ctx command origin]
-  (ud/dbug :receive {:origin origin} command)
-  (process command origin ctx))
-
 ;; use qualified symbols in case code is invoked
 ;; after calling (in-ns 'invalid-ns)
 
@@ -140,7 +136,7 @@ interpreted by the REPL client. The following specials are available:
   "Returns a function from blob (as string) and terminating? (an boolean atom)
    to streams pairs (as a map with keys :chars-out and :edn-in)."
   [host port]
-  (fn connect [blob terminating?]
+  (fn connect ([blob terminating?]
     (let [conn (.Socket. un/net)]
       {:chars-out conn
        :edn-in (-> (doto conn
@@ -156,7 +152,7 @@ interpreted by the REPL client. The following specials are available:
                                   (println "Socket error:" (pr-str err))
                                   (js/process.exit 1))))
                  (.pipe (uw/make-skip "[:unrepl/hello"))
-                 (.pipe (uw/make-edn-stream)))})))
+                 (.pipe (uw/make-edn-stream)))}))))
 
 (defn call-remote [{:keys [rl callbacks] :as ctx} form cb]
   (let [eval-id (str (gensym))
@@ -174,40 +170,40 @@ interpreted by the REPL client. The following specials are available:
   (or (some-> (some->> s (re-matches #"^(.{67})(.{3}).*$") second) (str "...")) s))
 
 (defn show-doc [{:keys [rl ostream state] :as ctx} full?]
-  (when-let [word (ul/find-word-at (.-line rl) (max 0 (dec (.-cursor rl))))]
-    (when (plausible-symbol? word)
-      (when (or (not= word (:word @state)) full?)
-        (swap! state assoc :word word)
-        (call-remote ctx
-                     (if full?
-                       (list '->>
-                             (list 'clojure.repl/doc (symbol word))
-                             'with-out-str)
-                       (list '->> (list 'clojure.repl/doc (symbol word))
-                             'with-out-str
-                             '(re-matches (re-pattern "(?is)(.*?\n(.*?\n)?(.*?\n)?(.*?\n)?)(.*)$"))
-                             'rest))
-                     (fn [r]
-                       (if full?
-                         (do
-                           (println)
-                           (.clearScreenDown ostream)
-                           (println r)
-                           (.prompt rl true))
-                         (let [[result more] r]
-                           (when result
-                             (let [pos (._getCursorPos rl)
-                                   lines (clojure.string/split-lines (cond-> (clojure.string/trimr result)
-                                                                       more
-                                                                       (str "...")))]
-                               (println)
-                               (doseq [line lines]
-                                 (.clearLine ostream)
-                                 (println (cut line 70)))
-                               (.moveCursor (js/require "readline")
-                                            (.-output rl)
-                                            (.-cols pos)
-                                            (- (+ (count lines) 1)))))))))))))
+  #_(when-let [word (ul/find-word-at (.-line rl) (max 0 (dec (.-cursor rl))))]
+     (when (plausible-symbol? word)
+       (when (or (not= word (:word @state)) full?)
+         (swap! state assoc :word word)
+         (call-remote ctx
+                      (if full?
+                        (list '->>
+                              (list 'clojure.repl/doc (symbol word))
+                              'with-out-str)
+                        (list '->> (list 'clojure.repl/doc (symbol word))
+                              'with-out-str
+                              '(re-matches (re-pattern "(?is)(.*?\n(.*?\n)?(.*?\n)?(.*?\n)?)(.*)$"))
+                              'rest))
+                      (fn [r]
+                        (if full?
+                          (do
+                            (println)
+                            (.clearScreenDown ostream)
+                            (println r)
+                            (.prompt rl true))
+                          (let [[result more] r]
+                            (when result
+                              (let [pos (._getCursorPos rl)
+                                    lines (clojure.string/split-lines (cond-> (clojure.string/trimr result)
+                                                                        more
+                                                                        (str "...")))]
+                                (println)
+                                (doseq [line lines]
+                                  (.clearLine ostream)
+                                  (println (cut line 70)))
+                                (.moveCursor (js/require "readline")
+                                             (.-output rl)
+                                             (.-cols pos)
+                                             (- (+ (count lines) 1)))))))))))))
 
 (defn complete [ctx line cb]
   (let [word (or (ul/find-word-at line (count line)) "")
@@ -249,75 +245,109 @@ interpreted by the REPL client. The following specials are available:
                        (set-prompt ctx nil warn?)
                        (.prompt rl true)))))))
 
+(defn- state-machine [state-map rf]
+  (let [state (volatile! state-map)
+        reentrant-calls (volatile! nil)
+        sm (fn [& args]
+             (if @reentrant-calls
+               (vswap! reentrant-calls conj args)
+               (try
+                 (vreset! reentrant-calls [])
+                 (vreset! state
+                   (loop [state (apply rf @state args)]
+                     (if-some [calls (seq @reentrant-calls)]
+                       (do (vreset! reentrant-calls [])
+                         (recur (reduce #(apply rf %1 %2) state calls)))
+                       state)))
+                 (finally
+                   (vreset! reentrant-calls nil)))))]
+    (vswap! state assoc :sm sm)
+    sm))
+
 (defn start [host port]
-  (let [istream js/process.stdin
-        ostream js/process.stdout
-        terminating? (atom false)
-        connect (socket-connector host port)
+  (let [connect (socket-connector host port)
+        ctx {:istream js/process.stdin
+             :ostream js/process.stdout
+             :terminating? (atom false)
+             :callbacks (atom {})
+             :pending-eval (atom nil)
+             :state (atom {})}
         conn-out (.Socket. un/net)
-        {conn-in :edn-in conn-out :chars-out} (connect (read-payload) terminating?)]
-    (.on conn-in
-         "started"
-         (fn [{:as session-info {:keys [start-aux :unrepl.jvm/start-side-loader]} :actions}]
-           (let [{aux-in :edn-in aux-out :chars-out} (connect (pr-str start-aux) terminating?)
-                 completer-fn (atom nil)]
-             (ud/dbug :main-connection-ready)
-             (.on aux-in
-                  "started"
-                  (fn []
-                    (ud/dbug :aux-connection-ready)
-                    (when (ut/interactive?)
-                      (banner host port))
-                    (.createInterface un/readline
-                                      #js{:input istream
-                                          :output ostream
-                                          :path (un/join-path (un/os-homedir) ".unravel" "history")
-                                          :maxLength 1000
-                                          :completer (fn [line cb]
-                                                       (when-let [f @completer-fn]
-                                                         (f line cb)))
-                                          :next (fn [rl]
-                                                  (let [ctx {:istream istream
-                                                             :ostream ostream
-                                                             :session-info session-info
-                                                             :callbacks (atom {})
-                                                             :pending-eval (atom nil)
-                                                             :conn-in conn-in
-                                                             :conn-out conn-out
-                                                             :aux-in aux-in
-                                                             :aux-out aux-out
-                                                             :terminating? terminating?
-                                                             :rl rl
-                                                             :state (atom {})}]
-                                                    (reset! completer-fn (partial complete ctx))
-                                                    (.on conn-in "data" #(did-receive ctx % :conn))
-                                                    (.on aux-in "data" #(did-receive ctx % :aux))
-                                                    (.on rl "line" (fn [line]
-                                                                     (when (ut/rich?)
-                                                                       (.clearLine ostream)
-                                                                       (.clearScreenDown ostream))
-                                                                     (if-let [[_ cmd] (re-matches #"^\s*#__([a-zA-Z0-9]*)?\s*$" line)]
-                                                                       (special ctx cmd)
-                                                                       (send-command ctx line))))
-                                                    (.on rl "close" (fn []
-                                                                      (when (ut/rich?)
-                                                                        (println))
-                                                                      (reset! terminating? true)
-                                                                      (ud/dbug :end "conn-out")
-                                                                      (.end conn-out)
-                                                                      (.end aux-out)))
-                                                    (.on rl "SIGINT" #(interrupt ctx))
-                                                    (.on istream "error"
-                                                         (fn []
-                                                           (.exit js/process 143)))
-                                                    (.on istream "keypress"
-                                                         (fn [chunk key]
-                                                           (cond
-                                                             (and (.-ctrl key) (= "o" (.-name key)))
-                                                             (do
-                                                               #_(check-readable ctx true)
-                                                               (show-doc ctx true))
-                                                             :else
-                                                             (do
-                                                               (check-readable ctx false)
-                                                               (show-doc ctx false)))))))}))))))))
+        {conn-in :edn-in conn-out :chars-out} (connect (read-payload) (:terminating? ctx))
+        sm (state-machine {:ctx (into ctx {:conn-in conn-in :conn-out conn-out})
+                           :pending []}
+             (fn self [{:keys [ctx sm] :as state} origin  [tag payload group-id :as msg]]
+               (ud/dbug :dispatch [origin tag])
+               ; at some point this case and process should merge
+               (case [origin tag]
+                 [:conn :unrepl/hello]
+                 (let [{:as session-info {:keys [start-aux :unrepl.jvm/start-side-loader]} :actions} payload
+                       {aux-in :edn-in aux-out :chars-out} (connect (pr-str start-aux) (:terminating? ctx))
+                       #_#_{loader-in :edn-in aux-out :loader-out} (connect (pr-str start-side-loader) (:terminating? ctx))]
+                   (ud/dbug :main-connection-ready)
+                   (.on aux-in "data" (fn [msg] (sm :aux msg)))
+                   (-> state
+                     (assoc :session-info session-info)
+                     (update :ctx into {:aux-in aux-in :aux-out aux-out})))
+                 [:aux :unrepl/hello]
+                 (let [{:as aux-session-info {:keys []} :actions} payload]
+                   ; TODO change string-length
+                   (ud/dbug :aux-connection-ready)
+                   (when (ut/interactive?)
+                     (banner host port))
+                   (.createInterface un/readline
+                     #js{:input (:istream ctx)
+                         :output (:ostream ctx)
+                         :path (un/join-path (un/os-homedir) ".unravel" "history")
+                         :maxLength 1000
+                         :completer (fn [line cb]
+                                      (when-let [f (:completer-fn state)]
+                                        (f line cb)))
+                         :next (fn [rl] (sm :readline [:ready rl]))})
+                   state)
+                 [:readline :ready]
+                 (let [rl payload
+                       state (reduce (fn [state [origin msg]] (self state origin msg))
+                               (-> state (update :ctx assoc :rl rl) (assoc :pending [] #_#_:completer-fn (partial complete ctx)))
+                               (:pending state))
+                       ctx (:ctx state)]
+                   (doto rl
+                     (.on "line" (fn [line]
+                                   (when (ut/rich?)
+                                     (doto (:ostream ctx) .clearLine .clearScreenDown))
+                                   (if-let [[_ cmd] (re-matches #"^\s*#__([a-zA-Z0-9]*)?\s*$" line)]
+                                     (special ctx cmd)
+                                     (send-command ctx line))))
+                     (.on "close" (fn []
+                                    (when (ut/rich?)
+                                      (println))
+                                    (reset! (:terminating? ctx) true)
+                                    (ud/dbug :end "conn-out")
+                                    (.end (:conn-out ctx))
+                                    (.end (:aux-out ctx))))
+                     (.on "SIGINT" #(interrupt ctx)))
+                   (doto (:istream ctx)
+                     (.on "error" #(.exit js/process 143))
+                     (.on "keypress"
+                       (fn [chunk key]
+                         (cond
+                           (and (.-ctrl key) (= "o" (.-name key)))
+                           (do
+                             #_(check-readable ctx true)
+                             (show-doc ctx true))
+                           :else
+                           (do
+                             (check-readable ctx false)
+                             (show-doc ctx false))))))
+                   state)
+                 ;default
+                 (do
+                   (if (:rl ctx)
+                     (do
+                       (ud/dbug :receive {:origin origin} command)
+                       (process msg origin ctx)
+                       state)
+                     (do
+                       (ud/dbug :rl-not-yet-initialized origin msg)
+                       (update state :pending conj [origin msg])))))))]
+    (.on conn-in "data" (fn [msg] (sm :conn msg)))))
